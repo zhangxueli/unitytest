f07ff3f9cfe28ca6ea6ab853d452880f416f9104
Luzz1987 <luzhe.03@163.com>
2017-06-15 10:35:02 +0800

    k k

---
+++ Assets/Editor/ExportAssetBundles.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class ExportAssetBundles : MonoBehaviour {
+
+	[MenuItem("Custom Editor/Buile AssetBunldes")]
+	static void CreateAssetBunldesMain()
+	{
+		//BuildPipeline.BuildAssetBundles ("Assets/SteamingAssets");
+		BuildPipeline.BuildAssetBundles("Assets/StreamingAssets", BuildAssetBundleOptions.None, BuildTarget.WebGL);
+	
+		if (BuildPipeline.BuildAssetBundles ("Assets/StreamingAssets", BuildAssetBundleOptions.None, BuildTarget.WebGL)) 
+		{
+			Debug.Log ("SUESSFUL!!!");
+		}
+	} 
+
+}
