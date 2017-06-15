fa0c2f3e334087c522582418243332c14e629a25
Luzz1987 <luzhe.03@163.com>
2017-06-15 14:01:54 +0800

    999 99

---
+++ Assets/Scripts/LoadScene01.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadScene01 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnClick_Back01load()
+	{
+		SceneManager.LoadScene ("01 load");	
+	}
+
+	void OnGUI()
+	{
+		if(GUI.Button (new Rect (20, 230, 200, 200), "Back To 01 load"))
+		{
+			SceneManager.LoadScene ("01 load");	
+		}
+	}
+}
